4c7e0cddda86f55440faa1524521e6d3a0a8eee4
Fran <fgg.tut@gmail.com>
2021-10-09 13:21:30 +0000

    fixed error from shellcheck

--- portscanner.sh
+++ portscanner.sh
@@ -24,7 +24,7 @@ openports()
   HOST=$(`which hostname`)
   NETSTAT_OUT=$(netstat -tuln4 | grep 'LISTEN' | awk '{print $4}' | grep 0.0.0.0 | cut -d':' -f2 | sort -un)
   mapfile -t portlist < <(echo "${NETSTAT_OUT}")
-  OPENPORTS=$(echo "[${portlist[@]}]" | tr " " ",")
+  OPENPORTS=$(echo "[${portlist[*]}]" | tr " " ",")
 
   if [ -z "$WHITELIST" ]
   then
